f819e2ef05bfcb53c45b033aa53859cb91e0da6a
jinseok7298 <dusengody@naver.com>
2021-07-14 15:34:23 +0900

    codeup 100

---
+++ codeup/1012.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    float a;
+    
+    cin>>a;
+    
+    cout<<fixed;
+    cout.precision(6);
+
+    cout<<a;
+    
+}
---
+++ codeup/1018.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n, m;
+    char a;
+    
+    cin>>n>>a>>m;
+    
+    cout<<n<<a<<m;
+    
+}
