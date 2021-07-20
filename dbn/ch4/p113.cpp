8690777ee28db6f739b69b743a57d92c68e3446b
jinseok7298 <dusengody@naver.com>
2021-07-20 16:25:25 +0900

    update answer

--- dbn/ch4/p113.cpp
+++ dbn/ch4/p113.cpp
@@ -19,10 +19,10 @@ int main(){
                 if((h % 10) == 3){
                     count += 1;
                     continue;
-                } else if((m % 10) == 3 or (m / 10) == 3){
+                } else if((m % 10) == 3 || (m / 10) == 3){
                     count += 1;
                     continue;
-                } else if((s % 10) == 3 or (s / 10) == 3){
+                } else if((s % 10) == 3 || (s / 10) == 3){
                     count += 1;
                     continue;
                 }
@@ -38,3 +38,34 @@ int main(){
 
     return 0;
 }
+
+
+//동빈씨 해설
+
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// int h, cnt;
+
+// // 특정한 시각 안에 '3'이 포함되어 있는지의 여부
+// bool check(int h, int m, int s) {
+//     if (h % 10 == 3 || m / 10 == 3 || m % 10 == 3 || s / 10 == 3 || s % 10 == 3)
+//         return true;
+//     return false;
+// }
+
+// int main(void) {
+//     // H를 입력받기 
+//     cin >> h;
+//     for (int i = 0; i <= h; i++) {
+//         for (int j = 0; j < 60; j++) {
+//             for (int k = 0; k < 60; k++) {
+//                 // 매 시각 안에 '3'이 포함되어 있다면 카운트 증가
+//                 if (check(i, j, k)) cnt++;
+//             }
+//         }
+//     }
+//     cout << cnt << '\n';
+//     return 0;
+// }
