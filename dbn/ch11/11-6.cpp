b9ba65c1cc9e444020a6c52bba3e0c62526c2119
jinseok7298 <dusengody@naver.com>
2021-09-03 14:46:00 +0900

    modify

--- dbn/ch11/11-6.cpp
+++ dbn/ch11/11-6.cpp
@@ -8,29 +8,29 @@ long long k;
 int solution(vector<int> food_times, long long k) {
     int answer = 0;
     long long times = 0;
-    int num = food_times.size();
-    int flag = 0;
+    int num = food_times.size();    //음식 수
+    int flag = 0;       //while문 종료를 위한 신호
 
     while (1){
-        int cnt = 0;
+        int cnt = 0;    //다 먹은 음식 수 세는 변수
 
         for (int i = 0; i < num; i++){
-            if (food_times[i] == 0) cnt += 1;
 
-            if (times == k && food_times[i] > 0) {
-                answer = i + 1;
-                flag = 1;
+            if (times == k && food_times[i] > 0) {      //현재 시간과 방송사고 시간 일치 & 먹을 음식 번호
+                answer = i + 1;     //방송 사고 후 먹을 음식 번호
+                flag = 1;       //while 종료를 위한 신호 값
                 break;
             }
-            else if (food_times[i] > 0) {
+            else if (food_times[i] == 0) cnt += 1;   //음식 다 먹었다면 먹은 음식 수 카운트
+            else if (food_times[i] > 0) {     //음식을 먹을 수 있을 경우
                 food_times[i] = food_times[i] - 1;
-                times += 1;
+                times += 1;     //음식 먹고 시간 +1
             }
         }
 
         if (flag == 1) break;
 
-        if (num == cnt) {
+        if (num == cnt) {       //음식수 = 다먹은 음식 수인 경우 종료
             answer = -1;
             break;
         }
