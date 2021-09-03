3dd5abdd25efb9a067b5c696b98b8ae9e189fa6c
jinseok7298 <dusengody@naver.com>
2021-09-03 14:31:15 +0900

    modify

--- dbn/ch11/11-1.cpp
+++ dbn/ch11/11-1.cpp
@@ -20,7 +20,7 @@ int main(){
     int cnt = 0;
     for(int i = 0; i < n; i++){
         cnt ++;     //꺼낸 모험가 수
-        if (fear[i] <= cnt){    //공포도와 꺼낸 모함가 수가 일치하면 그룹에 추가
+        if (fear[i] <= cnt){    //공포도보다 꺼낸 모험가 수가 많으면 그룹에 추가
             cnt = 0;    //꺼낸 모험가 수 초기화
             result += 1;
         }
